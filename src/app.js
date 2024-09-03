2b735cc6811cbe4a905b1f92197ae84c473f65a3
nilesh0863 <bhimawadnilesh04@gmail.com>
2024-09-03 13:53:19 +0530

    utils code add

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" })); // means that our app is accepting json and max limit is 16kb
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); // this used for url
+app.use(express.static("public"));
+app.use(cookieParser());
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 dotenv.config({ path: "./env" });
-connectDB();
+import app from "./app.js";
+
+// Use environment variable for port or default to 8000
+const PORT = process.env.PORT || 3000;
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERR:", error);
+      throw error;
+    });
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed:", err.message);
+    process.exit(1); // Exit the process with a failure code
+  });
